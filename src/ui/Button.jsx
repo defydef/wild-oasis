6813587e5bbc85e300c6689959c560f5a8d2230f
defy <8841170+defydef@users.noreply.github.com>
2023-10-09 21:08:08 +0700

    Create reusable Button component

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -1,24 +1,24 @@
 import styled, { css } from "styled-components";
 
-// const sizes = {
-//   small: css`
-//     font-size: 1.2rem;
-//     padding: 0.4rem 0.8rem;
-//     text-transform: uppercase;
-//     font-weight: 600;
-//     text-align: center;
-//   `,
-//   medium: css`
-//     font-size: 1.4rem;
-//     padding: 1.2rem 1.6rem;
-//     font-weight: 500;
-//   `,
-//   large: css`
-//     font-size: 1.6rem;
-//     padding: 1.2rem 2.4rem;
-//     font-weight: 500;
-//   `,
-// };
+const sizes = {
+  small: css`
+    font-size: 1.2rem;
+    padding: 0.4rem 0.8rem;
+    text-transform: uppercase;
+    font-weight: 600;
+    text-align: center;
+  `,
+  medium: css`
+    font-size: 1.4rem;
+    padding: 1.2rem 1.6rem;
+    font-weight: 500;
+  `,
+  large: css`
+    font-size: 1.6rem;
+    padding: 1.2rem 2.4rem;
+    font-weight: 500;
+  `,
+};
 
 const variations = {
   primary: css`
@@ -49,18 +49,17 @@ const variations = {
 };
 
 const Button = styled.button`
-  font-size: 1.4rem;
-  padding: 1.2rem 1.6rem;
-  font-weight: 500;
   border: none;
   border-radius: var(--border-radius-sm);
-  background-color: var(--color-brand-600);
-  color: var(--color-brand-50);
   box-shadow: var(--shadow-sm);
-  cursor: pointer;
-  &:hover {
-    background-color: var(--color-brand-700);
-  }
+
+  ${(props) => sizes[props.size]}
+  ${(props) => variations[props.variation]}
 `;
+
+Button.defaultProps = {
+  variation: "primary",
+  size: "medium",
+};
 
 export default Button;
