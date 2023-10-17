68c70af36bbe3f4c86243fdfb0e8c1664d0cae08
defy <8841170+defydef@users.noreply.github.com>
2023-10-17 07:53:38 +0700

    Implement updateSettings functionality

--- src/features/settings/UpdateSettingsForm.jsx
+++ src/features/settings/UpdateSettingsForm.jsx
@@ -4,6 +4,9 @@ import Input from "../../ui/Input";
 import useSettings from "./useSettings";
 import Spinner from "../../ui/Spinner";
 import ErrorFallback from "../../ui/ErrorFallback";
+import useUpdateSettings from "./useUpdateSettings";
+import { useForm } from "react-hook-form";
+import Button from "../../ui/Button";
 
 function UpdateSettingsForm() {
   const { isLoadingSettings, settings, retrieveSettingsError } = useSettings();
@@ -13,22 +16,62 @@ function UpdateSettingsForm() {
     maxGuestsPerBooking,
     breakfastPrice,
   } = settings || {};
+
+  const { register, handleSubmit, formState } = useForm();
+
+  const { isEditing, editSettings } = useUpdateSettings();
+
+  function onSubmit(data) {
+    editSettings(
+      { newSettings: { ...data } },
+      {
+        onSuccess: (data) => {
+          // console.log(data); // edited settings
+        },
+      }
+    );
+  }
+
+  function onError(errors) {
+    console.log(errors);
+  }
+
   if (isLoadingSettings) return <Spinner />;
   if (retrieveSettingsError)
     return <ErrorFallback error={retrieveSettingsError} />;
   return (
-    <Form>
+    <Form onSubmit={handleSubmit(onSubmit, onError)}>
       <FormRow label="Minimum nights/booking">
-        <Input type="number" id="min-nights" defaultValue={minBookingLength} />
+        <Input
+          type="number"
+          id="min-nights"
+          defaultValue={minBookingLength}
+          {...register("minBookingLength", {
+            required: "This field is required",
+          })}
+          disabled={isEditing}
+        />
       </FormRow>
       <FormRow label="Maximum nights/booking">
-        <Input type="number" id="max-nights" defaultValue={maxBookingLength} />
+        <Input
+          type="number"
+          id="max-nights"
+          defaultValue={maxBookingLength}
+          {...register("maxBookingLength", {
+            required: "This field is required",
+          })}
+          disabled={isEditing}
+        />
       </FormRow>
       <FormRow label="Maximum guests/booking">
         <Input
           type="number"
           id="max-guests"
           defaultValue={maxGuestsPerBooking}
+          {...register("maxGuestsPerBooking", {
+            required: "This field is required",
+          })}
+          disabled={isEditing}
         />
       </FormRow>
       <FormRow label="Breakfast price">
@@ -36,7 +79,14 @@ function UpdateSettingsForm() {
           type="number"
           id="breakfast-price"
           defaultValue={breakfastPrice}
+          {...register("breakfastPrice", {
+            required: "This field is required",
+          })}
+          disabled={isEditing}
         />
+      </FormRow>
+      <FormRow>
+        <Button>Update settings</Button>
       </FormRow>
     </Form>
   );
---
+++ src/features/settings/useUpdateSettings.js
@@ -0,0 +1,19 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { updateSetting } from "../../services/apiSettings";
+import toast from "react-hot-toast";
+
+function useUpdateSettings() {
+  const queryClient = useQueryClient();
+  const { isLoading: isEditing, mutate: editSettings } = useMutation({
+    mutationFn: ({ newSettings }) => updateSetting(newSettings),
+    onSuccess: () => {
+      toast.success("Settings successfully updated");
+      queryClient.invalidateQueries({ queryKey: ["settings"] }); // invalidate queries so React Query fetch latest data
+    },
+    onError: (err) => toast.error(err.message),
+  });
+
+  return { isEditing, editSettings };
+}
+
+export default useUpdateSettings;
